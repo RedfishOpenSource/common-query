bd6c892b6e5820bcb69a45eb9531ed92c17756b1
13396565428 <redfish_zly@163.com>
2024-04-24 21:24:27 +0800

    '自动同步'

---
+++ engine-mysql/src/test/java/com/redfish/common/query/engine/mysql/service/PageQueryServiceTest.java
@@ -0,0 +1,13 @@
+package com.redfish.common.query.engine.mysql.service;
+
+
+@EnableAutoConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RunWith(SpringRunner.class)
+public class PageQueryServiceTest {
+
+
+
+
+
+}
